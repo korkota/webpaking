a568b851cf33ef9069be67346f89ac206ae437c5
Alexey Korkota <alexey.korkota@gmail.com>
2017-03-19 21:57:18 +0300

    Add async loading for Momen.js with 'ru' locale

--- frontend/home.js
+++ frontend/home.js
@@ -1,8 +1,12 @@
 "use strict";
 
 import bar from './bar';
-import moment from 'moment';
 
-const now = moment(new Date()).locale('ru').format('LLLL');
+setTimeout(() => {
+  require.ensure([], () => {
+    let moment = require('moment');
+    let now = moment(new Date()).locale('ru').format('LLLL');
 
-alert(`${now}: home- ${bar()}`);
+    alert(`${now}: home- ${bar()}`);
+  })
+}, 5000 /*ms*/);
